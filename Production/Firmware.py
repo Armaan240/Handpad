4f687bd70c38a34a6bc5313823dbd89c1a2e5ca0
Armaan Soni <kiransonischool@gmail.com>
2025-12-21 16:32:48 +0530

    Refactor keyboard setup and integrate RGB features
    Updated the keyboard configuration and added RGB functionality.

--- Production/Firmware.py
+++ Production/Firmware.py
@@ -1,47 +1,59 @@
+print("Starting Macropad")
 import board
 from kmk.kmk_keyboard import KMKKeyboard
+from kmk.scanners.keypad import KeysScanner
 from kmk.keys import KC
-from kmk.scanners.digital import DigitalScanner
 from kmk.modules.encoder import EncoderHandler
-from kmk.modules.rgb import RGB
-from kmk.modules.display import Display
-from kmk.extensions.display.ssd1306 import SSD1306
+from kmk.extensions.rgb import RGB
+from kmk.extensions.rgb import AnimationModes
+
+
+# Keys
 keyboard = KMKKeyboard()
 
-keyboard.matrix = DigitalScanner(
-    pins=[board.GP4, board.GP2, board.GP1, board.GP0], 
-    pull=True
+PINS = [board.D7, board.D8, board.D9, board.D10]
+
+keyboard.matrix = KeysScanner(
+    pins=PINS,
+    value_when_pressed=False,
 )
-enc = EncoderHandler()
-enc.pins = [
-    (board.GP26, board.GP27, None)
+
+encoder_handler = EncoderHandler()
+keyboard.modules.append(encoder_handler)
+
+encoder_handler.pins = (
+    (board.D0, board.D1, None), 
+)
+
+encoder_handler.map = [
+    ((KC.LCTL(KC.EQUAL), KC.LCTL(KC.MINUS)),),
 ]
-enc.map = [
-    ((KC.VOLU,), (KC.VOLD,)),
-]
-keyboard.modules.append(enc)
+
 rgb = RGB(
-    pixel_pin=board.GP28,  
-    num_pixels=6,          
-    val_limit=50           
-)
-keyboard.modules.append(rgb)
-oled = Display(
-    driver=SSD1306(
-        width=128,
-        height=64,
-        i2c=board.I2C(),  
-        addr=0x3C
-    )
-)
-keyboard.modules.append(oled)
+    pixel_pin=board.D6,  
+    num_pixels=6,
+    val_limit=100,
+    hue_default=150,
+    sat_default=200,
+    rgb_order=(1, 0, 2),  # GRB WS2812
+    val_default=100,
+    hue_step=5,
+    sat_step=5,                                      
+    val_step=5,
+    animation_speed=2,
+    breathe_center=2,  # 1.0-2.7
+    knight_effect_length=3,
+    animation_mode=AnimationModes.STATIC,
+    reverse_animation=False,
+    refresh_rate=60,)
+
+keyboard.extensions.append(rgb)
+# Keymap
 keyboard.keymap = [
-    [
-        KC.A, 
-        KC.W,  
-        KC.S, 
-        KC.D,  
-    ]
+    [KC.A, KC.W, KC.S , KC.RGB_MODE_RAINBOW]
 ]
+
+# Start everything
 if __name__ == '__main__':
+    print("Keyboard starting...")
     keyboard.go()
